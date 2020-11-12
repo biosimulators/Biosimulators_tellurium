2fb358676075156848b276c030213ddb325a2370
Jonathan Karr <jonrkarr@gmail.com>
2020-11-12 17:41:30 -0500

    linting

--- biosimulators_tellurium/__main__.py
+++ biosimulators_tellurium/__main__.py
@@ -32,7 +32,7 @@ class BaseController(cement.Controller):
 
     @cement.ex(hide=True)
     def _default(self):
-        args = self.app.pargs        
+        args = self.app.pargs
         try:
             exec_combine_archive(args.archive, args.out_dir)
         except Exception as exception:
