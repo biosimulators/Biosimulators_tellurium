02f9786fd55b79d4cf1f42cd4986e7ab07381864
Jonathan Karr <jonrkarr@gmail.com>
2021-08-13 16:38:47 -0400

    feat: added method to get the version of tellurium

--- biosimulators_tellurium/__init__.py
+++ biosimulators_tellurium/__init__.py
@@ -1,4 +1,15 @@
+import tellurium
+
 from ._version import __version__  # noqa: F401
 # :obj:`str`: version
 
 from .core import exec_sedml_docs_in_combine_archive  # noqa: F401
+
+
+def get_simulator_version():
+    """ Get the version of tellurium
+
+    Returns:
+        :obj:`str`: version
+    """
+    return tellurium.__version__
--- biosimulators_tellurium/__main__.py
+++ biosimulators_tellurium/__main__.py
@@ -6,13 +6,13 @@
 :License: MIT
 """
 
+from . import get_simulator_version
 from ._version import __version__
 from .core import exec_sedml_docs_in_combine_archive
 from biosimulators_utils.simulator.cli import build_cli
-import tellurium
 
 App = build_cli('biosimulators-tellurium', __version__,
-                'tellurium', tellurium.__version__, 'http://tellurium.analogmachine.org',
+                'tellurium', get_simulator_version(), 'http://tellurium.analogmachine.org',
                 exec_sedml_docs_in_combine_archive)
 
 
