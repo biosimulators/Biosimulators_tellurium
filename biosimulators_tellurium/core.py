7e8cc5f7bd7aa62c464f4a7de6a88cbae869dfcd
Lucian Smith <lpsmith@uw.edu>
2023-08-15 14:22:31 -0700

    Use isinstance correctly (sigh).

--- biosimulators_tellurium/core.py
+++ biosimulators_tellurium/core.py
@@ -311,10 +311,10 @@ def exec_sed_task(task, variables, preprocessed_task=None, log=None, config=None
 
 def get_all_tasks_from_task(task):
     ret = set()
-    if task.isinstance(Task):
+    if isinstance(task, Task):
         ret.add(task)
         return ret
-    elif task.isinstance(RepeatedTask):
+    elif isinstance(task, RepeatedTask):
         for sub_task in task.sub_tasks:
             submodels = get_all_tasks_from_task(sub_task.task)
             ret.update(submodels)
